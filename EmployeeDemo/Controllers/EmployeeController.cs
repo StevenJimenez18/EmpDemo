29b5d28fab53cd7e84561ae9f24afd6b7713f9da
Steven Jimenez <steven.jimenez2029@gmail.com>
2021-09-21 11:18:53 -0400

    Employee Demo Commit

---
+++ EmployeeDemo/Controllers/EmployeeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeDemo.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Employeenames = new string[] { "Elana", "Sarah", "Logan", "Nathan", "Michael" };
+            return View();
+        }
+
+        public IActionResult Details(string empname)
+        {
+            ViewBag.Emp = empname;
+            return View();
+        }
+    }
+}
